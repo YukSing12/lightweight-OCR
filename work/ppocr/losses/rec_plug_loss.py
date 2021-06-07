7d074b4c3a7d5e6e10d71f608b9de8916a439866
YukSing12 <39983134+YukSing12@users.noreply.github.com>
2021-06-07 23:24:42 +0800

    Add pluggable super-resolution unit

---
+++ work/ppocr/losses/rec_plug_loss.py
@@ -0,0 +1,67 @@
+# copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import paddle
+from paddle import nn
+import paddle.nn.functional as F
+import numpy as np
+from paddle import ParamAttr
+from paddle.fluid.initializer import NumpyArrayInitializer
+class PlugLoss(nn.Layer):
+    def __init__(self, **kwargs):
+        super(PlugLoss, self).__init__()
+        self.rec_loss_func = nn.CTCLoss(blank=0, reduction='none')
+        self.sr_loss_func = nn.L1Loss(reduction='mean')
+        self.lambda_factor = 0.01 # balance the weight in two different tasks and keep the recognition performance 
+        self.alpha = 0.5 # probability parameter alpha
+
+        # build gaussian_blur by convolution
+        gaussian_blur_filter = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype='float32')
+        gaussian_blur_filter = np.repeat(gaussian_blur_filter.reshape([1, 1, 3, 3]), 3, axis=1)
+        self.gaussian_blur = nn.Conv2D(in_channels=3, out_channels=3, kernel_size=[3, 3],padding='SAME',
+                  weight_attr=ParamAttr(
+                      name='gaussian_blur',
+                      initializer=NumpyArrayInitializer(value=gaussian_blur_filter),
+                      trainable=False
+                  ))
+
+    def __call__(self, predicts, batch, frn_blur_I):
+        frn_I = predicts[1]
+        predicts = predicts[0]
+
+        predicts = predicts.transpose((1, 0, 2))
+        N, B, _ = predicts.shape
+        preds_lengths = paddle.to_tensor([N] * B, dtype='int64')
+        labels = batch[1].astype("int32")
+        label_lengths = batch[2].astype('int64')
+        rec_loss = self.rec_loss_func(predicts, labels, preds_lengths, label_lengths).mean()
+
+        sr_loss = self.sr_loss_func(frn_I, frn_blur_I)
+
+        loss = rec_loss + self.lambda_factor * sr_loss  # sum
+        return {'loss': loss}
+
+    def get_blur(self,I,p1,p2):
+        if p1 >= self.alpha and p2 >= self.alpha:
+            return F.upsample(F.upsample(self.gaussian_blur(I),scale_factor=1/4,mode='nearest'),scale_factor=4,mode='nearest')
+        elif p1 >= self.alpha and p2 < self.alpha:
+            return self.gaussian_blur(I)
+        elif p1 < self.alpha and p2 >= self.alpha:
+            return F.upsample(F.upsample(I,scale_factor=1/4,mode='nearest'),scale_factor=4,mode='nearest')
+        else:
+            return I
---
+++ work/ppocr/modeling/necks/rcan.py
@@ -0,0 +1,166 @@
+# Reference from https://github.com/huiyang865/plugnet/blob/master/models/rcan.py
+
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+import math
+
+__all__ = ['RCAN']
+
+def make_model(args, parent=False):
+    return RCAN(args)
+
+def default_conv(in_channels, out_channels, kernel_size):
+    return nn.Conv2D(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2))
+
+class Upsampler(nn.Sequential):
+    def __init__(self, conv, scale, n_feat, bn=False, act=False):
+
+        m = []
+        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
+            for _ in range(int(math.log(scale, 2))):
+                m.append(conv(n_feat, 4 * n_feat, 3))
+                m.append(nn.PixelShuffle(2))
+                if bn: m.append(nn.BatchNorm2D(n_feat))
+                if act: m.append(act())
+        elif scale == 3:
+            m.append(conv(n_feat, 9 * n_feat, 3))
+            m.append(nn.PixelShuffle(3))
+            if bn: m.append(nn.BatchNorm2D(n_feat))
+            if act: m.append(act())
+        else:
+            raise NotImplementedError
+
+        super(Upsampler, self).__init__(*m)
+
+## Channel Attention (CA) Layer
+class CALayer(nn.Layer):
+    def __init__(self, channel, reduction=16):
+        super(CALayer, self).__init__()
+        # global average pooling: feature --> point
+        self.avg_pool = nn.AdaptiveAvgPool2D(1)
+        # feature channel downscale and upscale --> channel weight
+        self.conv_du = nn.Sequential(
+                nn.Conv2D(channel, channel // reduction, 1, padding=0),
+                nn.ReLU(),
+                nn.Conv2D(channel // reduction, channel, 1, padding=0),
+                nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+## Residual Channel Attention Block (RCAB)
+class RCAB(nn.Layer):
+    def __init__(
+        self, conv, n_feat, kernel_size, reduction,
+        bn=False, act=nn.ReLU(True), res_scale=1):
+
+        super(RCAB, self).__init__()
+        modules_body = []
+        for i in range(2):
+            modules_body.append(conv(n_feat, n_feat, kernel_size))
+            if bn: modules_body.append(nn.BatchNorm(n_feat))
+            if i == 0: modules_body.append(act)
+        modules_body.append(CALayer(n_feat, reduction))
+        self.body = nn.Sequential(*modules_body)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x)
+        # res = self.body(x).mul(self.res_scale)
+        res += x
+        return res
+
+## Residual Group (RG)
+class ResidualGroup(nn.Layer):
+    def __init__(self, conv, n_feat, kernel_size, reduction, act, res_scale, n_resblocks):
+        super(ResidualGroup, self).__init__()
+        modules_body = []
+        modules_body = [
+            RCAB(
+                conv, n_feat, kernel_size, reduction, bn=False, act=nn.ReLU(True), res_scale=1) \
+            for _ in range(n_resblocks)]
+        modules_body.append(conv(n_feat, n_feat, kernel_size))
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        res = self.body(x)
+        res += x
+        return res
+
+## Residual Channel Attention Network (RCAN)
+class RCAN(nn.Layer):
+    def __init__(self, in_channels, conv=default_conv):
+        super(RCAN, self).__init__()
+        
+        # n_resgroups = 10
+        # n_resblocks = 20
+        n_resgroups = 2
+        n_resblocks = 2
+        n_feats = 64
+        kernel_size = 3
+        reduction = 16
+        scale = 4
+        act = nn.ReLU(True)
+
+        # define body module
+        modules_body = [
+            ResidualGroup(
+                conv, n_feats, kernel_size, reduction, act=act, res_scale=1, n_resblocks=n_resblocks) \
+            for _ in range(n_resgroups)]
+
+        modules_body.append(conv(n_feats, n_feats, kernel_size))
+
+        # define tail module
+        modules_tail = [
+            Upsampler(conv, scale, n_feats, act=False),
+            conv(n_feats, 3, kernel_size)]
+
+        self.conv1 = conv(in_channels, n_feats, kernel_size=1)
+        self.body = nn.Sequential(*modules_body)
+        self.tail = nn.Sequential(*modules_tail)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        res = self.body(x)
+        res += x
+
+        x = self.tail(res)
+
+        return x 
+
+    def load_state_dict(self, state_dict, strict=False):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                if isinstance(param, nn.Parameter):
+                    param = param.data
+                try:
+                    own_state[name].copy_(param)
+                except Exception:
+                    if name.find('tail') >= 0:
+                        print('Replace pre-trained upsampler to new one...')
+                    else:
+                        raise RuntimeError('While copying the parameter named {}, '
+                                           'whose dimensions in the model are {} and '
+                                           'whose dimensions in the checkpoint are {}.'
+                                           .format(name, own_state[name].size(), param.size()))
+            elif strict:
+                if name.find('tail') == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'
+                                   .format(name))
+
+        if strict:
+            missing = set(own_state.keys()) - set(state_dict.keys())
+            if len(missing) > 0:
+                raise KeyError('missing keys in state_dict: "{}"'.format(missing))
+
+if __name__ == '__main__':
+    rcan = RCAN(in_channels=3)
+    paddle.summary(rcan, (1,3,32,320))
+    pass
